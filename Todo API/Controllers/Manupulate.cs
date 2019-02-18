c766bd888c55c8cf2108a12e91941588320de4bc
94ju <janithherath94@gmail.com>
2019-02-18 09:55:04 +0530

    Manupulating data

--- Todo API/Controllers/Manupulate.cs
+++ Todo API/Controllers/Manupulate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TodoAPI.Models;
 
-namespace Todo API.Controllers
+namespace TodoAPI.Controllers
 {
     public class Manupulate
     {
@@ -12,7 +12,13 @@ namespace Todo API.Controllers
 
         public static void getStringncheck(List<TodoItem> todoItem)
         {
+            List<String> names = new List<string>();
+            foreach (TodoItem todoitems in todoItem) {
+                names.Add(todoitems.Name);
+            }
+            
             Console.WriteLine(todoItem);
+            Console.WriteLine(names);
             //throw new NotImplementedException();
         }
     }
--- Todo API/Controllers/TodoController.cs
+++ Todo API/Controllers/TodoController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Todo;
+//using Todo;
 using TodoApi.Models;
 using TodoAPI.Models;
 
