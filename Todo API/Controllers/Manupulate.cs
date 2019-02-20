826df3e02daab49b3498f101365fd815a71353a5
94ju <janithherath94@gmail.com>
2019-02-20 13:20:22 +0530

    Difference taken

--- Todo API/Controllers/Manupulate.cs
+++ Todo API/Controllers/Manupulate.cs
@@ -17,19 +17,38 @@ namespace TodoAPI.Controllers
             foreach (TodoItem todoitems in todoItem) {
                 names.Add(todoitems.Name);
             }
-            string stringName = string.Join(",", names);
-            // string[] authorsList = stringName.Split(", ");
+           string stringName = string.Join(",", names[0]);
+            string stringName2 = string.Join(",", names[1]);
+            //  names[0] = names[0].Split(", ");
 
-            stringName=String.Concat(stringName," This is th name list");
-            todoItem[0].Name = stringName;
-            //TodoItem todoitem = new TodoItem()
-            //{
-            //    Name = stringName 
-                
+            List<int[]> nameList = new List<int[]>();
+            List<string> diff;
+            IEnumerable<string> set1 = stringName.Split(',').Distinct();
+            IEnumerable<string> set2 = stringName2.Split(',').Distinct();
+
+            if (set2.Count() > set1.Count())
+            {
+                diff = set2.Except(set1).ToList();
+            }
+            else
+            {
+                diff = set1.Except(set2).ToList();
+            }
+            //string namess = onlyInArray2
+            // nameList.Add(names.ToList);
+            // Console.WriteLine(onlyInArray2);
+            // stringName =String.Concat(stringName," This is th name list");
+            todoItem[1].Name = string.Join(",", diff);
+          //  names = onlyInArray2.ToString()     ;
+          //  stringName = null;
+          //TodoItem todoitem = new TodoItem()
+          //{
+          //    Name = stringName 
+
             //};
             //transferData.PostTodoItem();
             //authorsList = authorsList[0] + ;
-           // Console.WriteLine(authorsList);
+            // Console.WriteLine(authorsList);
             //Console.WriteLine(todoItem);
             //Console.WriteLine(names);
             //throw new NotImplementedException();
